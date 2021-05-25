1479cdc5d92b41b20169adda20c9b52c1872a690
zfletch <zfletch@users.noreply.github.com>
2021-05-25 21:36:39 +0000

    Deploying to gh-pages from  @ f65a3033a1c332bf0bf1f7ad9e71e8e3199ee148 🚀

---
+++ precache-manifest.93b67fd24ea49cfeb45c649adb146b25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb82ca1762909aa23bf3f1ade13a42a",
+    "url": "/treebank-template/index.html"
+  },
+  {
+    "revision": "0d128bb6e132d89fe4be",
+    "url": "/treebank-template/static/css/2.354f031a.chunk.css"
+  },
+  {
+    "revision": "599cfa479cf4d0447716",
+    "url": "/treebank-template/static/css/main.bd98509e.chunk.css"
+  },
+  {
+    "revision": "0d128bb6e132d89fe4be",
+    "url": "/treebank-template/static/js/2.92dd7ecd.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/treebank-template/static/js/2.92dd7ecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599cfa479cf4d0447716",
+    "url": "/treebank-template/static/js/main.94b2cdc1.chunk.js"
+  },
+  {
+    "revision": "fc82e1a45c5889b39b98",
+    "url": "/treebank-template/static/js/runtime-main.94c54c7b.js"
+  }
+]);
