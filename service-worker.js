bf2e9ebc5e863c47032c55be2c1297cae8ffe11b
zfletch <zfletch@users.noreply.github.com>
2021-05-25 21:42:38 +0000

    Deploying to gh-pages from  @ 0d035d04388caea37eaeb3049a0a98c479becb8f 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/treebank-template/precache-manifest.93b67fd24ea49cfeb45c649adb146b25.js"
+  "/unesp-trees/precache-manifest.07c2b02f435ecba603102cc90957fe9e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/treebank-template/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unesp-trees/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
